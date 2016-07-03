daad36b9d57cf61d422872c49dbc26b0a254cd80
kylekmorris <kylemorris555@gmail.com>
2016-07-03 16:26:18 +0200

    added files to web

---
+++ web/server.js
@@ -0,0 +1,21 @@
+
+var express = require('express');
+var app = express();
+
+// express.js config
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+
+app.listen(8080);
